d3b2c687992fff32842ad6b6fe5861d796b9d75c
Valera Gorobets <valera.gorobets@gmail.com>
2018-03-01 10:11:44 +0300

    Create start layout

---
+++ src/Components/Main/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Main extends Component {
+  render() {
+    return ( 
+      <div className="container">
+        <aside>
+          aside
+        </aside>
+        <section>
+          section
+        </section>
+      </div>
+    );
+  }
+}
